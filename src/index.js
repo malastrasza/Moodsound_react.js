74e7663a26f97bb23ee7e4542cb5b422bab9c152
malastrasza <malastrasza@gmail.com>
2018-12-18 11:35:48 +0100

    Pokazywanie nowych stron po kliknieciu w przyciski

--- src/index.js
+++ src/index.js
@@ -16,30 +16,86 @@ const firstPageDivButtonStyle2 = {
 
 class Main extends React.Component {
 
-    handleButtonGood = () => {
-        console.log("Good")
+    handleButtonGoodProps = () => {
+      this.props.ClickMethodGood();
     };
-    handleButtonNeutral = () => {
-        console.log("Neutral")
+    handleButtonNeutralProps = () => {
+        this.props.ClickMethodNeutral();
     };
-    handleButtonBad = () => {
-        console.log("Bad")
+    handleButtonBadProps = () => {
+        this.props.ClickMethodBad();
     };
 
+
     render() {
-        return <div style={firstPageDivButtonStyle1}>
+        return <div id="firstPage" style={firstPageDivButtonStyle1}>
             <div><p>Jak się czujesz?</p></div>
             <div style={firstPageDivButtonStyle2}>
-                <button onClick={this.handleButtonGood}>Good</button>
-                <button onClick={this.handleButtonNeutral}>Neutral</button>
-                <button onClick={this.handleButtonBad}>Bad</button>
+                <button onClick={this.handleButtonGoodProps}>Good</button>
+                <button onClick={this.handleButtonNeutralProps}>Neutral</button>
+                <button onClick={this.handleButtonBadProps}>Bad</button>
             </div>
         </div>
     }
 }
 
+class SecondPageGood extends React.Component {
+    render() {
+        return <div>SECOND PAGE GOOD</div>
+    }
+}
+
+class SecondPageNeutral extends React.Component {
+    render() {
+        return <div>SECOND PAGE NEUTRAL</div>
+    }
+}
+
+class SecondPageBad extends React.Component {
+    render() {
+        return <div>SECOND PAGE BAD</div>
+    }
+}
+
+class Page extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            page: "1",
+        }
+    }
+
+    handleButtonGood = () => {
+        console.log("Good");
+        this.setState({
+            page: "2good"
+        })
+    };
+    handleButtonNeutral = () => {
+        this.setState({
+            page: "2neutral"
+        })
+    };
+    handleButtonBad = () => {
+        this.setState({
+            page: "2bad"
+        })
+    };
+
+    render() {
+        if (this.state.page == 1) {
+            return <Main ClickMethodGood={this.handleButtonGood} ClickMethodNeutral={this.handleButtonNeutral} ClickMethodBad={this.handleButtonBad}/>
+        } else if (this.state.page == "2good") {
+            return <SecondPageGood/>
+        } else if (this.state.page == "2neutral") {
+            return <SecondPageNeutral/>
+        } else if (this.state.page == "2bad") {
+            return <SecondPageBad/> }
+    }
+}
+
 function App() {
-    return <Main/>
+    return <Page/>
 }
 
 document.addEventListener('DOMContentLoaded', function () {
