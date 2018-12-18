0c6f4100f4e5c8216469cd6bd12794aeb078aa4c
malastrasza <malastrasza@gmail.com>
2018-12-18 09:43:06 +0100

    Dodane buttony i eventy do buttonow

--- src/index.js
+++ src/index.js
@@ -1,17 +1,45 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+const firstPageDivButtonStyle1 = {
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+    flexDirection: "column",
+    width: "100vw",
+    height: "100vh"
+};
 
-class HelloName extends React.Component {
+const firstPageDivButtonStyle2 = {
+    display: "flex",
+};
+
+class Main extends React.Component {
+
+    handleButtonGood = () => {
+        console.log("Good")
+    };
+    handleButtonNeutral = () => {
+        console.log("Neutral")
+    };
+    handleButtonBad = () => {
+        console.log("Bad")
+    };
+
     render() {
-        return <div>
-            Hello
+        return <div style={firstPageDivButtonStyle1}>
+            <div><p>Jak się czujesz?</p></div>
+            <div style={firstPageDivButtonStyle2}>
+                <button onClick={this.handleButtonGood}>Good</button>
+                <button onClick={this.handleButtonNeutral}>Neutral</button>
+                <button onClick={this.handleButtonBad}>Bad</button>
+            </div>
         </div>
     }
 }
 
 function App() {
-    return <HelloName/>
+    return <Main/>
 }
 
 document.addEventListener('DOMContentLoaded', function () {
