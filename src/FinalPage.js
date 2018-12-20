f09299d3804231ae664bf373e17312858c37a752
malastrasza <malastrasza@gmail.com>
2018-12-20 15:09:01 +0100

    zaimplementowalem react youtube

--- src/FinalPage.js
+++ src/FinalPage.js
@@ -1,5 +1,6 @@
 import React from "react";
 import firebase, {db} from "./firebase";
+import YouTube from 'react-youtube';
 
 const finalDiv = {
     backgroundColor: "#DAAE90",
@@ -24,85 +25,64 @@ class FinalPage extends React.Component {
         }
     }
 
-componentWillMount() {
+    componentWillMount() {
 
-    let answer = this.props.drawIndex;
-    console.log(answer);
+        let answer = this.props.drawIndex;
+        console.log(answer);
 
-    if (answer[0] == 10 && answer[1] == 200) {
-        this.setState({
-            response: "redclouds"
-        })
+        if (answer[0] == 10 && answer[1] == 200) {
+            this.setState({
+                response: "darkmatter"
+            })
+        } else if (answer[0] == 10 && answer [1] == 201) {
+            this.setState({
+                response: "strainofhate"
+            })
+        }
     }
-}
 
     componentDidMount() {
 
         db.collection('songs').doc(this.state.response).get().then((doc) => {
             if (doc.exists) {
-                this.setState({album: doc.data().album})
+                this.setState({
+                        artist: doc.data().artist,
+                        artwork: doc.data().artwork,
+                        title: doc.data().title,
+                        url: doc.data().url
+                    }
+                )
             } else {
                 console.log("No such document!");
             }
         }).catch(function (error) {
             console.log("Error getting document:", error);
         });
-
-        db.collection('songs').doc(this.state.response).get().then((doc) => {
-            if (doc.exists) {
-                this.setState({artist: doc.data().artist})
-            } else {
-                console.log("No such document!");
-            }
-        }).catch(function (error) {
-            console.log("Error getting document:", error);
-        });
-
-        db.collection('songs').doc(this.state.response).get().then((doc) => {
-            if (doc.exists) {
-                this.setState({artwork: doc.data().artwork})
-            } else {
-                console.log("No such document!");
-            }
-        }).catch(function (error) {
-            console.log("Error getting document:", error);
-        });
-
-        db.collection('songs').doc(this.state.response).get().then((doc) => {
-            if (doc.exists) {
-                this.setState({title: doc.data().title})
-            } else {
-                console.log("No such document!");
-            }
-        }).catch(function (error) {
-            console.log("Error getting document:", error);
-        });
-
-        db.collection('songs').doc(this.state.response).get().then((doc) => {
-            if (doc.exists) {
-                this.setState({url: doc.data().url})
-            } else {
-                console.log("No such document!");
-            }
-        }).catch(function (error) {
-            console.log("Error getting document:", error);
-        });
-
-
 
     }
 
     render() {
-        return <div style={finalDiv}>
-            <div><h1>PIOSENKA DLA CIEBIE:</h1></div>
-            <div><img src={this.state.artwork} alt="album_artwork" height="250" width="250"/></div>
-            <div><h1>{this.state.title}</h1></div>
-            <div><h3>{this.state.artist}</h3></div>
-            <div><h3>{this.state.album}</h3></div>
-            <iframe id="ytplayer" type="text/html" width="640" height="360"
-                    src="https://www.youtube.com/embed/iw-b_VdpCEU?autoplay=1&start=7"
-                    frameBorder="0"/>
-        </div>
+        {
+            const opts = {
+                height: '390',
+                width: '640',
+                playerVars: {
+                    autoplay: 1,
+                }
+            };
+            return <div style={finalDiv}>
+                <div><h1>PIOSENKA DLA CIEBIE:</h1></div>
+                <div><img src={this.state.artwork} alt="album_artwork" height="250" width="250"/></div>
+                <div><h1>{this.state.title}</h1></div>
+                <div><h3>{this.state.artist}</h3></div>
+
+                <YouTube
+                    videoId={this.state.url}
+                    opts={opts}
+                />
+
+            </div>
+        }
     }
 }
 
